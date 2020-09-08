70bd136c4a4bf8979cfed25ff48e688626523ee7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-08 13:53:48 +0530

    adding constatns

---
+++ index.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const parts : number = 5  
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const delay : number = 20
+const rFactor : number = 12.2  
+const lineFactor : number = 8 
+const colors : Array<string> = ["indigo", "green", "purple", "teal", "#ff5722"]
+const backColor : string = "#BDBDBD"
